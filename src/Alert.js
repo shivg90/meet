86b1c647c9461ac138058e1484e164c988bb6bc2
shivg90 <siobhan.gatenby1@live.co.uk>
2023-03-17 16:54:15 +0100

    adjustments

--- src/Alert.js
+++ src/Alert.js
@@ -38,7 +38,7 @@ class ErrorAlert extends Alert {
     }
 }
 
-class WarningAlert extends Alert { // defined alert but not using yet
+class WarningAlert extends Alert { 
     constructor(props) {
         super(props);
         this.color = 'orange';
